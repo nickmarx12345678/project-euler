af677147386ccfe3a3e99196b6e5b54356485740
Patrick Marx <patrick.k.marx@gmail.com>
2015-06-11 13:26:25 -0700

    sped up alg by removing implicit factors

---
+++ problem5.py
@@ -0,0 +1,14 @@
+current = 20
+divisors = [11,12,13,14,15,16,17,18,19]
+divisors.reverse()
+while True:
+    found = True
+    for i in divisors:
+        if not current % i == 0:
+            found = False
+            break
+    if found:
+        print current
+        break
+    else:
+        current += 20
